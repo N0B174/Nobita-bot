44ad37f1049fee5f6b8b08153fb6cd43c71f9813
N0B174 <j3rrydcw@gmail.com>
2025-04-21 21:07:18 +0600

    Update ｢File｣__Adminupdate.js

--- Script/commands/｢File｣__Adminupdate.js
+++ Script/commands/｢File｣__Adminupdate.js
@@ -1,7 +1,7 @@
 module.exports.config = {
  name: "adminupdate",
  version: "1.0.0",
- credits: "Islamick Cyber Chat",
+ credits: "𝐘𝐨𝐮𝐫 𝐗𝐚𝐧 😚😘🥰",
  hasPermssion: 2,
  description: "\Toggle group information update messages",
  usages: "",
